9af137d869cb048a12afc139b628e4b07d59c282
hmueller96 <hanmueller96@gmail.com>
2022-03-27 21:08:03 -0500

    Lab 06

---
+++ apps/Lab06WorldTrotter/Lab06WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab06WorldTrotter
+//
+//  Created by Hannah Mueller on 3/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
